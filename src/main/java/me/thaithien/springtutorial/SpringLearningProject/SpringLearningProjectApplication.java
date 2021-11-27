83a87900caafc888172dc13ffc98a186ceedebc8
Thai Thien <tthien@apcs.vn>
2021-11-27 15:17:46 +0700

    add all

---
+++ src/main/java/me/thaithien/springtutorial/SpringLearningProject/SpringLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package me.thaithien.springtutorial.SpringLearningProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/thaithien/springtutorial/SpringLearningProject/SpringLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thaithien.springtutorial.SpringLearningProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
